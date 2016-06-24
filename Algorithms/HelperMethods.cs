6fa214296cd460cc30f38b67db8f1ce7d2e7fec7
Tarek Mamdouh <tarek.mamdouh.cis@gmail.com>
2016-06-24 14:23:41 +0200

    Add files via upload

---
+++ Algorithms/HelperMethods.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGUtilities
+{
+    public class HelperMethods
+    {
+        public static Enums.PointInPolygon PointInTriangle(Point p, Point a, Point b, Point c)
+        {
+            if (a.Equals(b) && b.Equals(c))
+            {
+                if (p.Equals(a) || p.Equals(b) || p.Equals(c))
+                    return Enums.PointInPolygon.OnEdge;
+                else
+                    return Enums.PointInPolygon.Outside;
+            }
+
+            Line ab = new Line(a, b);
+            Line bc = new Line(b, c);
+            Line ca = new Line(c, a);
+
+            if (GetVector(ab).Equals(Point.Identity)) return (PointOnSegment(p, ca.Start, ca.End)) ? Enums.PointInPolygon.OnEdge : Enums.PointInPolygon.Outside;
+            if (GetVector(bc).Equals(Point.Identity)) return (PointOnSegment(p, ca.Start, ca.End)) ? Enums.PointInPolygon.OnEdge : Enums.PointInPolygon.Outside;
+            if (GetVector(ca).Equals(Point.Identity)) return (PointOnSegment(p, ab.Start, ab.End)) ? Enums.PointInPolygon.OnEdge : Enums.PointInPolygon.Outside;
+
+            if (CheckTurn(ab, p) == Enums.TurnType.Colinear)
+                return PointOnSegment(p, a, b)? Enums.PointInPolygon.OnEdge : Enums.PointInPolygon.Outside;
+            if (CheckTurn(bc, p) == Enums.TurnType.Colinear && PointOnSegment(p, b, c))
+                return PointOnSegment(p, b, c) ? Enums.PointInPolygon.OnEdge : Enums.PointInPolygon.Outside;
+            if (CheckTurn(ca, p) == Enums.TurnType.Colinear && PointOnSegment(p, c, a))
+                return PointOnSegment(p, a, c) ? Enums.PointInPolygon.OnEdge : Enums.PointInPolygon.Outside;
+
+            if (CheckTurn(ab, p) == CheckTurn(bc, p) && CheckTurn(bc, p) == CheckTurn(ca, p))
+                return Enums.PointInPolygon.Inside;
+            return Enums.PointInPolygon.Outside;
+        }
+        public static Enums.TurnType CheckTurn(Point vector1, Point vector2)
+        {
+            double result = CrossProduct(vector1, vector2);
+            if (result < 0) return Enums.TurnType.Right;
+            else if (result > 0) return Enums.TurnType.Left;
+            else return Enums.TurnType.Colinear;
+        }
+        public static double distance(Point a, Point b)
+        {
+            return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+        }
+        public static double LinePointDist(Line ab, Point c)
+        {
+            Point v1 = ab.Start.Vector(ab.End);
+            Point v2 = ab.End.Vector(c);
+            double projDistance = Math.Abs(dot(v2, v1) / v1.Magnitude());
+            double diag = ab.End.Vector(c).Magnitude();
+            return Math.Sqrt(diag * diag - projDistance * projDistance);
+        }
+        public static double getAngle(Point a, Point b, Point c)
+        {
+            if (a.Equals(b) || b.Equals(c))
+                return 10;
+            Point VecB = a.Vector(b);
+            Point VecC = b.Vector(c);
+            double ang = Math.Acos(dot(VecB, VecC) / (VecB.Magnitude() * VecC.Magnitude()));
+            double angB = Math.Atan2(VecB.Y, VecB.X);
+            double angC = Math.Atan2(VecC.Y, VecC.X);
+            angB += (angB < 0 ? 2 * Math.PI : 0);
+            angC += (angC < 0 ? 2 * Math.PI : 0);
+            if (Math.Abs(angC - angB - ang) > 1e-6)
+                ang = ang - 2;
+            ang = (angC - angB)+((angC - angB) < 0 ? 2 * Math.PI : 0);
+            return ang;
+        }
+        public static double dot(Point v1, Point v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y;
+        }
+        public static double CrossProduct(Point a, Point b)
+        {
+            return a.X * b.Y - a.Y * b.X;
+        }
+        public static bool PointOnLine(Point p, Point a, Point b)
+        {
+            return Math.Abs(distance(a, p) + distance(b, p) - distance(a, b)) < Constants.Epsilon;
+        }
+        public static bool PointOnRay(Point p, Point a, Point b)
+        {
+            if (a.Equals(b)) return true;
+            if (a.Equals(p)) return true;
+            var q = a.Vector(p).Normalize();
+            var w = a.Vector(b).Normalize();
+            return q.Equals(w);
+        }
+        public static bool PointOnSegment(Point p, Point a, Point b)
+        {
+            if (a.Equals(b))
+                return p.Equals(a);
+
+            if (b.X == a.X)
+                return p.X == a.X && (p.Y >= Math.Min(a.Y, b.Y) && p.Y <= Math.Max(a.Y, b.Y));
+            if (b.Y == a.Y)
+                return p.Y == a.Y && (p.X >= Math.Min(a.X, b.X) && p.X <= Math.Max(a.X, b.X));
+            double tx = (p.X - a.X) / (b.X - a.X);
+            double ty = (p.Y - a.Y) / (b.Y - a.Y);
+
+            return (Math.Abs(tx - ty) <= Constants.Epsilon && tx <= 1 && tx >= 0);
+        }
+        /// <summary>
+        /// Get turn type from cross product between two vectors (l.start -> l.end) and (l.end -> p)
+        /// </summary>
+        /// <param name="l"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static Enums.TurnType CheckTurn(Line l, Point p)
+        {
+            Point a = l.Start.Vector(l.End);
+            Point b = l.End.Vector(p);
+            return HelperMethods.CheckTurn(a, b);
+        }
+        public static Point GetVector(Line l)
+        {
+            return l.Start.Vector(l.End);
+        }
+    }
+}
